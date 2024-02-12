e8921c75acec7cdaae2eda47fb9780e3f96b8062
Yash Grover <83108253+theyashgrover@users.noreply.github.com>
2024-02-12 18:18:06 +0530

    Adding Video Model and User Model with JWT,Aggregate and bcrypt package

---
+++ src/models/video.model.js
@@ -0,0 +1,42 @@
+import mongoose,{Schema, mongo} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+    videoFile : {
+        type:String, //cloudinary url
+        required:true
+    },
+    thumbnail : {
+        type:String, //cloudinary url
+        required:true
+    },
+    title : {
+        type:String,
+        required:true
+    },
+    description : {
+        type:String,
+        required:true
+    },
+    duration : {
+        type:Number,
+        required:true
+    },
+    views : {
+        type:Number,
+        default:0
+    },
+    isPublished : {
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+//to use aggregation pipelines we will need to do the following :
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video",videoSchema);
